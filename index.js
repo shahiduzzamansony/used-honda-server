ed39e060ad1085706d33e5bcc0193b2f2c456ccf
Your shahiduzzamansony <shahiduzzamansony2703@gmail.com>
2022-11-25 20:12:57 +0600

    users got

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run() {
       res.send(products);
     });
 
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
