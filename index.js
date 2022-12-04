f7f859c95ec082d68b900667bf219694ab16e6ad
Your shahiduzzamansony <shahiduzzamansony2703@gmail.com>
2022-12-04 15:52:26 +0600

    repo changed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 const app = express();
-
+// middle wares
 app.use(cors());
 app.use(express.json());
 
