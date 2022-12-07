c887fb6551331afc83b353d49dd6409c8cdab838
Your shahiduzzamansony <shahiduzzamansony2703@gmail.com>
2022-12-08 02:52:29 +0600

    ok

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 const app = express();
-// middle wares
+
 app.use(cors());
 app.use(express.json());
 
@@ -70,13 +70,15 @@ async function run() {
       const result = await productsCollection.findOne(query);
       res.send(result);
     });
-    app.get("/users", verifyJWT, async (req, res) => {
-      let query = {};
-      if (req.query.role) {
-        query = {
-          role: req.query.role,
-        };
-      }
+    app.get("/users/buyer", verifyJWT, async (req, res) => {
+      const query = { role: "Buyer" };
+
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/users/seller", verifyJWT, async (req, res) => {
+      const query = { role: "Seller" };
+
       const result = await usersCollection.find(query).toArray();
       res.send(result);
     });
@@ -115,8 +117,11 @@ async function run() {
     });
 
     app.get("/users/role/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { email: email };
+      const decodedEmail = req.params.email;
+      // console.log(decodedEmail);
+
+      // const email = req.params.email;
+      const query = { email: decodedEmail };
       const result = await usersCollection.findOne(query);
       res.send(result);
     });
@@ -197,6 +202,7 @@ async function run() {
     });
     app.put("/products/verify/:email", async (req, res) => {
       const email = req.params.email;
+      // console.log(email);
       const filter = { email: email };
       const options = { upsert: true };
       const updatedDoc = {
